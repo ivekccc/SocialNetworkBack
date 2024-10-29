6317eb19303a891af363e7a4d5c2eae8e6778f9c
Ivan <ivanjelisavcic6@gmail.com>
2024-10-29 09:49:46 +0100

    First commit

---
+++ src/main/java/com/ivan/SocialNetworkBack/SocialNetworkBackApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.SocialNetworkBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkBackApplication.class, args);
+	}
+
+}
